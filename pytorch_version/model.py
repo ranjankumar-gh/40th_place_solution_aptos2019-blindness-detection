0fe40ea8b4fe25ce59f313285d87233197949e9e
ranjankumar-gh <mail4ranjan@gmail.com>
2021-08-26 08:55:45 +0000

    initial commit

--- pytorch_version/model.py
+++ pytorch_version/model.py
@@ -7,7 +7,8 @@ def DRModel(device):
     #model = torchvision.models.resnet101(pretrained=True)
     model = torchvision.models.resnet101(pretrained=False)
     # model.load_state_dict(torch.load("../input/pytorch_version-pretrained-models/resnet101-5d3b4d8f.pth"))
-    model.load_state_dict(torch.load("drive/MyDrive/projects/DR/aptos_best_model.pth"))
+    
+    model.load_state_dict(torch.load("drive/MyDrive/projects/DR/aptos_best_model.pth")['model_state_dict'])
     num_features = model.fc.in_features
     model.fc = nn.Linear(2048, 1)
     model = model.to(device)
